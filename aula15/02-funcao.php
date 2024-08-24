9fca34f2b774527c04f43ef61543942c31865dce
Emy-Liberato <paulo.alvares@sptech.school>
2024-08-24 18:22:51 -0300

    feat: functions pt.2

---
+++ aula15/02-funcao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../_css/estilo.css" />
+    <meta charset="UTF-8" />
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+    <div>
+        <?php
+            include "funcoes.php";
+            // include "funcoes2.php"; // Continua o código
+            // require "funcoes2.php"; // Para o código
+
+            echo "<h1>Testando novas funções</h1>";
+
+            ola();
+            mostraValor(4);
+
+            echo "<h2>Finalizando Programa...</h2>";
+        ?>
+    </div>
+</body>
+</html>
